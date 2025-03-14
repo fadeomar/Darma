9027c4459aac6aae8199275e58328844c5599c05
fadeomar <fadeomar2015@gmail.com>
2025-03-15 01:20:13 +0200

    create back button

---
+++ components/BackButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function BackButton() {
+  const router = useRouter();
+
+  return (
+    <button
+      onClick={() => router.back()}
+      className="mb-4 inline-flex items-center text-blue-600 hover:text-blue-800 transition-colors"
+    >
+      <svg
+        xmlns="http://xmlns.org/3.org/2000/svg"
+        className="h-5 w-5 mr-1"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
+          clipRule="evenodd"
+        />
+      </svg>
+      Back
+    </button>
+  );
+}

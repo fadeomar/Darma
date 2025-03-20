b65a1485cd263b5042ebe68caf7acfba992e66b5
fadeomar <fadeomar2015@gmail.com>
2025-03-20 16:38:10 +0200

    direct element route to search page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/element", // The route you don’t have a page for
+        destination: "/search", // The page you want to render
+      },
+    ];
+  },
 };
 
 export default nextConfig;
